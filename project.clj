7e6d31375fa94b89cf79fb07d9c87266aa3dfd97
Stan Rozenraukh <stan@stanistan.com>
2014-02-16 19:57:59 -0500

    Update proj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject crela "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Relational Webcrawler"
+  :url "http://github.com/stanistan/crela"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
